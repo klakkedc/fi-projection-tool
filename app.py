8212e07f36e7795e7223c1798f3c3fb4911ccd7b
klakkedc <Klaas.decuyper@outlook.com>
2025-12-05 13:30:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if "portfolio_df" not in st.session_state:
     st.session_state["portfolio_df"] = pd.DataFrame(
         [
             {"Ticker": "VWCE.DE", "Shares": 10.0, "Price": 0.0, "Value": 0.0, "Class": "Stocks"},
-            {"Ticker": "AGGH.DE", "Shares": 10.0, "Price": 0.0, "Value": 0.0, "Class": "Bonds"},
+            {"Ticker": "MWRD.PA", "Shares": 10.0, "Price": 0.0, "Value": 0.0, "Class": "Bonds"},
         ]
     )
 
@@ -1186,3 +1186,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
